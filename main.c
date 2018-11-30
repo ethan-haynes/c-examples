84b2c0d26978dedd280d55897948c1b64c918b35
Ethan Haynes <eghaynes17@gmail.com>
2018-11-29 21:18:16 -0600

    adding examples for a program in c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+int main() {
+    stack *stk = NULL;
+    push(7, &stk);
+    push(2, &stk);
+    push(9, &stk);
+    push(12,&stk);
+    push(15,&stk);
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    return 0;
+}
